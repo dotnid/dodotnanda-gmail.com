1484c9c8e28ce254163aa825a4f4a4ca1286c23f
Glitch (apple-nutritious-magnesium) <none>
2020-03-24 09:07:41 +0000

    📕🎪 Checkpoint ./public/index.html:12934572/94 ./public/scripts/install.js:12934572/1658 ./public/scripts/service-worker.js:12934572/3152

--- public/scripts/service-worker.js
+++ public/scripts/service-worker.js
@@ -1,21 +1,6 @@
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
   '/offline.html',
-];
-// CODELAB: Precache static resources here.
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-);
-
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v1';
-
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/scripts/app.js',
@@ -39,32 +24,41 @@ const FILES_TO_CACHE = [
   '/images/tornado.svg',
   '/images/wind.svg',
 ];
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
 
-
-  // CODELAB: Add fetch event handler here.
-  if (evt.request.url.includes('/forecast/')) {
-    console.log('[Service Worker] Fetch (data)', evt.request.url);
-    evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then((cache) => {
-          return fetch(evt.request)
-              .then((response) => {
-                // If the response was good, clone it and store it in the cache.
-                if (response.status === 200) {
-                  cache.put(evt.request.url, response.clone());
-                }
-                return response;
-              }).catch((err) => {
-                // Network request failed, try to get it from the cache.
-                return cache.match(evt.request);
-              });
-        }));
-    return;
-  }
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+// CODELAB: Add fetch event handler here.
+if (evt.request.url.includes('/forecast/')) {
+  console.log('[Service Worker] Fetch (data)', evt.request.url);
   evt.respondWith(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.match(evt.request)
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
             .then((response) => {
-              return response || fetch(evt.request);
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            }).catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
             });
-      })
-  );
+      }));
+  return;
+}
+evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request)
+          .then((response) => {
+            return response || fetch(evt.request);
+          });
+    })
+);
